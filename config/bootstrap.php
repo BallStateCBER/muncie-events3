227a5f578a081df2f4fa472cd2bbe04bb46350bc
Erica Dee Fox <ericadeefox@gmail.com>
2017-07-07 16:24:15 -0400

    Fix UTC/EDT confusion in ics & google exports

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -80,7 +80,7 @@ if (Configure::read('debug')) {
  * Set server timezone to UTC. You can change it to another timezone of your
  * choice but using UTC makes time calculations / conversions easier.
  */
-date_default_timezone_set('America/Indiana/Indianapolis');
+date_default_timezone_set('US/Eastern');
 
 /*
  * Configure the mbstring extension to use the correct encoding.
