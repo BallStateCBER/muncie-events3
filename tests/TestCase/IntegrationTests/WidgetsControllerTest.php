32fc208381c1276f827e0dfcd1359d9e69014889
Erica Dee Fox <ericadeefox@gmail.com>
2017-11-02 16:55:42 -0400

    Readd widgets tests

---
+++ tests/TestCase/IntegrationTests/WidgetsControllerTest.php
@@ -0,0 +1,147 @@
+<?php
+namespace App\Test\TestCase\Controller;
+
+use App\Test\TestCase\ApplicationTest;
+
+class WidgetsControllerTest extends ApplicationTest
+{
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+    }
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+    /**
+     * Test feed customizer method
+     *
+     * @return void
+     */
+    public function testFeedCustomizer()
+    {
+        $dummyEvent = [
+            'title' => 'Widget!',
+            'category_id' => 1,
+            'date' => date('m/d/Y'),
+            'time_start' => date('Y-m-d'),
+            'time_end' => strtotime('+1 hour'),
+            'location' => 'PlaceholderTown',
+            'location_details' => 'Room 6',
+            'address' => '666 Placeholder Place',
+            'description' => 'Come out with my support!',
+            'cost' => '$6',
+            'age_restriction' => '66 or younger',
+            'source' => 'Placeholder Digest Tri-Weekly',
+            'data' => [
+                'Tags' => [
+                    1
+                ]
+            ]
+        ];
+        $this->post('/events/add', $dummyEvent);
+        $this->assertResponseSuccess();
+        $dummyEvent = [
+            'title' => 'Twidget!',
+            'category_id' => 2,
+            'date' => date('m/d/Y'),
+            'time_start' => date('Y-m-d'),
+            'time_end' => strtotime('+1 hour'),
+            'location' => 'PlaceholderTown',
+            'location_details' => 'Room 6',
+            'address' => '666 Placeholder Place',
+            'description' => 'Come out with my support!',
+            'cost' => '$6',
+            'age_restriction' => '66 or younger',
+            'source' => 'Placeholder Digest Tri-Weekly',
+            'data' => [
+                'Tags' => [
+                    1
+                ]
+            ]
+        ];
+        $this->post('/events/add', $dummyEvent);
+        $this->assertResponseSuccess();
+        $dummyEvent = [
+            'title' => 'Twidget!',
+            'category_id' => 1,
+            'date' => date('m/d/Y'),
+            'time_start' => date('Y-m-d'),
+            'time_end' => strtotime('+1 hour'),
+            'location' => 'PlaceholderTown',
+            'location_details' => 'Room 6',
+            'address' => '666 Placeholder Place',
+            'description' => 'Come out with my support!',
+            'cost' => '$6',
+            'age_restriction' => '66 or younger',
+            'source' => 'Placeholder Digest Tri-Weekly',
+            'data' => [
+                'Tags' => [
+                    1
+                ]
+            ]
+        ];
+        $this->post('/events/add', $dummyEvent);
+        $this->assertResponseSuccess();
+        $dummyEvent = [
+            'title' => 'Twidget!',
+            'category_id' => 2,
+            'date' => date('m/d/Y'),
+            'time_start' => date('Y-m-d'),
+            'time_end' => strtotime('+1 hour'),
+            'location' => 'Mr. Placeholder\'s Casa',
+            'location_details' => 'Room 6',
+            'address' => '666 Placeholder Place',
+            'description' => 'Come out with my support!',
+            'cost' => '$6',
+            'age_restriction' => '66 or younger',
+            'source' => 'Placeholder Digest Tri-Weekly',
+            'data' => [
+                'Tags' => [
+                    1
+                ]
+            ]
+        ];
+        $this->post('/events/add', $dummyEvent);
+        $this->assertResponseSuccess();
+        $this->get('/widgets/feed?', [
+            'category' => 2,
+            'location' => 'placeholdertown',
+            'tags_included' => 'holding places',
+            'tags_excluded' => ''
+        ]);
+        $this->assertResponseOk();
+        $iframeQueryString = $this->viewVariable('iframeQueryString');
+        $this->markTestIncomplete();
+        #$this->assertEquals('category=2&location=placeholdertown&tags_included=holding%20places', $iframeQueryString);
+    }
+    /**
+     * Test month customizer method
+     *
+     * @return void
+     */
+    public function testMonthCustomizer()
+    {
+        $this->get('/widgets/month' . '?hideGeneralEventsIcon=1&category=2&location=placeholdertown&tags_included=holding%20places');
+        $this->assertResponseOk();
+        /*    $iframeQueryString =  $this->viewVariable('iframeQueryString');
+            $this->assertEquals('hideGeneralEventsIcon=1&category=13&location=placeholdertown&tags_included=potluck&tags_excluded=slow+food', $iframeQueryString); */
+        $dummies = $this->Events->find()
+            ->where(['title' => 'Widget!'])
+            ->orWhere(['title' => 'Twidget!']);
+        foreach ($dummies as $dummy) {
+            $this->Events->delete($dummy);
+        }
+        $this->markTestIncomplete();
+    }
+}
