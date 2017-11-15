8e104cbec5398bceb262a39d65ea260e42f6c9b5
Graham Watson <graham@phantomwatson.com>
2017-11-15 17:01:53 -0500

    Force HTTPS for all requests

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -114,6 +114,8 @@ class AppController extends Controller
                 'authorize' => 'Controller'
             ]
         );
+        $this->loadComponent('Security', ['blackHoleCallback' => 'forceSSL']);
+
         if (php_sapi_name() != 'cli') {
             $this->loadComponent('Captcha.Captcha');
             /*if (!in_array($this->request->getParam('action'), $this->autoComplete)) {
@@ -305,5 +307,15 @@ class AppController extends Controller
         );
 
         return $events;
+    }
+
+    /**
+     * Redirects the current request to HTTPS
+     *
+     * @return \Cake\Http\Response
+     */
+    public function forceSSL()
+    {
+        return $this->redirect('https://' . env('SERVER_NAME') . $this->request->getRequestTarget());
     }
 }
