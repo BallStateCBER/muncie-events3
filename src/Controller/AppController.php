45c941dc85ddf3a30af574c07b82262d075695bb
Erica Dee Fox <ericadeefox@gmail.com>
2017-12-21 20:19:09 -0500

    Update location string for emojione plugin

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -47,7 +47,7 @@ class AppController extends Controller
             'distribution' => 'basic',
             'local_plugin' => [
                 'emojione' => [
-                    'location' => WWW_ROOT . 'js',
+                    'location' => WWW_ROOT . 'emojione' . DS . 'lib' . DS . 'js',
                     'file' => 'emojione.js'
                 ]
             ],
