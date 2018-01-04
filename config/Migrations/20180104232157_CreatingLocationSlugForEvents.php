276dbe369014350432f9383d3fde26c27f3a66db
Erica Dee Fox <ericadeefox@gmail.com>
2018-01-04 18:42:27 -0500

    Create slug migration, issue #25

---
+++ config/Migrations/20180104232157_CreatingLocationSlugForEvents.php
@@ -0,0 +1,48 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreatingLocationSlugForEvents extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function up()
+    {
+        $events = $this->table('events');
+        $events
+            ->addColumn('location_slug', 'string', ['after' => 'location_details', 'limit' => 20, 'null' => false])
+            ->save();
+
+        $stmt = $this->query('SELECT * FROM events');
+        $events = $stmt->fetchAll();
+
+        foreach ($events as $event) {
+            $id = $event['id'];
+            $locationSlug = strtolower($event['location']);
+            $locationSlug = substr($locationSlug, 0, 20);
+            $locationSlug = preg_replace("/[^A-Za-z0-9 ]/", '', $locationSlug);
+            $locationSlug = str_replace("  ", ' ', $locationSlug);
+            $locationSlug = str_replace(' ', '-', $locationSlug);
+            $this->execute('UPDATE events SET location_slug="' . $locationSlug . '" WHERE id="' . $id . '"');
+        }
+    }
+
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function down()
+    {
+        $events = $this->table('events');
+        $events
+            ->removeColumn('location_slug')
+            ->save();
+    }
+}
