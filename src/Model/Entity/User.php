2322324f916e1a6b0ee73aca446af56d09914b6a
Graham Watson <graham@phantomwatson.com>
2018-01-29 16:12:40 -0500

    Fix error preventing any passwords from being saved

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -19,8 +19,7 @@ class User extends Entity
      */
     protected $_accessible = [
         '*' => true,
-        'id' => false,
-        'password' => false
+        'id' => false
     ];
 
     /**
