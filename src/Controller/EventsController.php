cf30271d878f489204bccbbf37ba67065780788c
Graham Watson <graham@phantomwatson.com>
2018-01-30 18:23:23 -0500

    Fix duplicated line

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -144,7 +144,6 @@ class EventsController extends AppController
                 $newTag = $this->Events->Tags->newEntity();
                 $newTag->name = $customTag;
                 $newTag->user_id = $this->Auth->user('id');
-                $newTag->user_id = $this->Auth->user('id');
                 $newTag->parent_id = 1012; // 'Unlisted' group
                 $newTag->listed = $this->Auth->user('role') == 'admin' ? 1 : 0;
                 $newTag->selectable = 1;
