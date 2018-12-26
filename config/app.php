bc784eb0449b391cbb5e201ccc48f3b3eaab0179
Graham Watson <graham@phantomwatson.com>
2018-12-26 15:40:12 -0500

    Stop redundantly loading .env in app.php
    Since it's already being loaded in bootstrap.php

--- config/app.php
+++ config/app.php
@@ -1,23 +1,4 @@
 <?php
-// Load variables from .env file
-try {
-    josegonzalez\Dotenv\Loader::load([
-        'filepath' => __DIR__ . DS . '.env',
-        'toServer' => false,
-        'skipExisting' => ['toServer'],
-        'raiseExceptions' => true
-    ]);
-} catch (InvalidArgumentException $e) {
-    // If there's a problem loading the .env file - load .env.default
-    // That means the code can assume appropriate env config always exists
-    // Don't trap this incase there's some other fundamental error
-    josegonzalez\Dotenv\Loader::load([
-        'filepath' => __DIR__ . DS . '.env.default',
-        'toServer' => false,
-        'skipExisting' => ['toServer'],
-        'raiseExceptions' => false
-    ]);
-}
 $config = [
     /**
      * Debug Level:
