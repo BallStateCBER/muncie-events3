dd8e043fa4943ae8e85af54c6e586f127826da2a
Graham Watson <graham@phantomwatson.com>
2018-12-28 17:15:37 -0500

    Copy migration files from Muncie Events API
    So migration status will stop complaining about them being missing

---
+++ config/Migrations/20171212220442_CreateApiCalls.php
@@ -0,0 +1,41 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateApiCalls extends AbstractMigration
+{
+    /**
+     * Up method
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->table('api_calls')
+            ->addColumn('user_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => false,
+            ])
+            ->addColumn('url', 'text', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->create();
+    }
+
+    /**
+     * Down method
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->dropTable('api_calls');
+    }
+}
