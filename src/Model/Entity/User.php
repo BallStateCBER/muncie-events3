b02331b8812b7efcb880c03da53e1662d995b620
Graham Watson <graham@phantomwatson.com>
2019-01-09 12:34:44 -0500

    Hide Users.password from JSON entities

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -42,6 +42,15 @@ class User extends Entity
     ];
 
     /**
+     * Fields that are excluded from JSON versions of the entity.
+     *
+     * @var array
+     */
+    protected $_hidden = [
+        'password'
+    ];
+
+    /**
      * Hashes the user's password if it doesn't match App.spamPassword
      *
      * @param string $password The plaintext password
